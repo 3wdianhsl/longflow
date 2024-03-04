5e86191f58276e3e32e2f095f91df6ebb6770112
hsl <1140606871@qq.com>
2024-03-04 14:51:08 +0800

    初始化

---
+++ src/components/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>headbar</div>
+        <div class="main">
+            <div>elementmenu</div>
+            <div>canvas</div>
+            <div>elementdetail</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.main{
+    display: flex;
+}
+</style>
